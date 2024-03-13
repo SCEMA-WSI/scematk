7fd1237d6632554adae05e1a6469a250b745f9dd
Hugh Warden <hugh.warden@outlook.com>
2024-03-13 18:18:53 +0000

    update dirs

--- src/scematk/__init__.py
+++ src/scematk/__init__.py
@@ -2,7 +2,7 @@
 #   Copyright (c) Microsoft Corporation. All rights reserved.
 #   Licensed under the MIT License. See LICENSE in project root for information.
 #   -------------------------------------------------------------
-"""Python Package Template"""
+"""scematk"""
 from __future__ import annotations
 
 __version__ = "0.0.2"
