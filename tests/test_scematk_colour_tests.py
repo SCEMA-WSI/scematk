5c3247f07b69071739585abf304b5b4bf8bd0da6
Hugh Warden <hugh.warden@outlook.com>
2024-03-14 09:00:18 +0000

    update tests

--- tests/test_scematk_colour_tests.py
+++ tests/test_scematk_colour_tests.py
@@ -4,8 +4,8 @@ from skimage.color import rgb2gray
 
 def test_rgb_to_grey():
     rimg = da.random.randint(0, 255, size=(500, 500, 3), chunks=(50, 50, 3))
-    diff = rgb_to_grey(rimg).compute() - rgb2gray(rimg.compute())
-    assert (diff < 1e-10).all()
+    diff = rgb_to_grey(rimg).compute() == rgb2gray(rimg.compute())
+    assert diff.all()
 
 def test_int_rgb_to_grey():
     rimg = da.random.randint(0, 255, size=(500, 500, 3), chunks=(50, 50, 3), dtype='int')
