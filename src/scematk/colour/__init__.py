242d4f261d368f11afceef02e5bf0786ae9eaaf1
Hugh Warden <hugh.warden@outlook.com>
2024-03-15 09:43:23 +0000

    force transforms to be uint8

--- src/scematk/colour/__init__.py
+++ src/scematk/colour/__init__.py
@@ -1,11 +1,9 @@
 from ._grey_transform import rgb_to_grey, rgb_to_gray
-from ._od_transform import grey_to_od, gray_to_od, od_to_grey, od_to_gray, rgb_to_od
+from ._od_transform import grey_to_od, gray_to_od, rgb_to_od
 
 __all__ = [
     "gray_to_od",
     "grey_to_od",
-    "od_to_gray",
-    "od_to_grey",
     "rgb_to_gray",
     "rgb_to_grey",
     "rgb_to_od"
--- src/scematk/colour/_od_transform.py
+++ src/scematk/colour/_od_transform.py
@@ -1,32 +1,19 @@
 import dask.array as da
 from dask.array import Array
 from scematk.colour import rgb_to_grey
-from skimage import img_as_ubyte
 
-def grey_to_od(image: Array, clip = True) -> Array:
+def grey_to_od(image: Array) -> Array:
     assert image.ndim == 2, "Input image must be 2D"
-    assert image.dtype in ["float32", "float64", "uint8"], "Input image must be float32, float64 or uint8"
-    if image.dtype == "uint8":
-        image = image / 255
-    od_img = -da.log10(image)
-    if clip:
-        od_img = da.clip(od_img, 0, 1)
+    assert image.dtype == "uint8", "Input image must be uint8"
+    od_img = -da.log10(image / 255)
+    od_img = da.clip(od_img, 0, 1)
+    od_img = da.round(od_img * 255, 0)
+    od_img = od_img.astype("uint8")
     return od_img
 
-def gray_to_od(image: Array, clip = True) -> Array:
-    return grey_to_od(image, clip)
+def gray_to_od(image: Array) -> Array:
+    return grey_to_od(image)
 
-def od_to_grey(image: Array, as_ubyte = True) -> Array:
-    assert image.ndim == 2, "Input image must be 2D"
-    assert image.dtype in ["float32", "float64"], "Input image must be float32 or float64"
-    grey_img = da.exp(-image)
-    if as_ubyte:
-        grey_img = da.map_blocks(img_as_ubyte, grey_img)
-    return grey_img
-
-def od_to_gray(image: Array, as_ubyte = True) -> Array:
-    return od_to_grey(image, as_ubyte)
-
-def rgb_to_od(image: Array, clip = True) -> Array:
+def rgb_to_od(image: Array) -> Array:
     image = rgb_to_grey(image)
-    return grey_to_od(image, clip)
+    return grey_to_od(image)
