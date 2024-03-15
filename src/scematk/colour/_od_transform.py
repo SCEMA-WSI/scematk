ceac554cb8b9f4bac298dd5603d052a29ba0d8a4
Hugh Warden <hugh.warden@outlook.com>
2024-03-15 07:48:37 +0000

    change colour defaults

--- src/scematk/colour/_od_transform.py
+++ src/scematk/colour/_od_transform.py
@@ -3,7 +3,7 @@ from dask.array import Array
 from scematk.colour import rgb_to_grey
 from skimage import img_as_ubyte
 
-def grey_to_od(image: Array, clip = False) -> Array:
+def grey_to_od(image: Array, clip = True) -> Array:
     assert image.ndim == 2, "Input image must be 2D"
     assert image.dtype in ["float32", "float64", "uint8"], "Input image must be float32, float64 or uint8"
     if image.dtype == "uint8":
@@ -13,10 +13,10 @@ def grey_to_od(image: Array, clip = False) -> Array:
         od_img = da.clip(od_img, 0, 1)
     return od_img
 
-def gray_to_od(image: Array, clip = False) -> Array:
+def gray_to_od(image: Array, clip = True) -> Array:
     return grey_to_od(image, clip)
 
-def od_to_grey(image: Array, as_ubyte = False) -> Array:
+def od_to_grey(image: Array, as_ubyte = True) -> Array:
     assert image.ndim == 2, "Input image must be 2D"
     assert image.dtype in ["float32", "float64"], "Input image must be float32 or float64"
     grey_img = da.exp(-image)
@@ -24,9 +24,9 @@ def od_to_grey(image: Array, as_ubyte = False) -> Array:
         grey_img = da.map_blocks(img_as_ubyte, grey_img)
     return grey_img
 
-def od_to_gray(image: Array, as_ubyte = False) -> Array:
+def od_to_gray(image: Array, as_ubyte = True) -> Array:
     return od_to_grey(image, as_ubyte)
 
-def rgb_to_od(image: Array, clip = False) -> Array:
+def rgb_to_od(image: Array, clip = True) -> Array:
     image = rgb_to_grey(image)
     return grey_to_od(image, clip)
