a9ea0a18813241972e060a47d96d43c97016393c
Hugh Warden <hugh.warden@outlook.com>
2024-03-15 10:02:21 +0000

    remove colour codes

--- src/scematk/preprocess/__init__.py
+++ src/scematk/preprocess/__init__.py
@@ -1,19 +1,7 @@
-from ._colour import (
-    RGBToGrey,
-    RGBToGray,
-    GreyToOD,
-    GrayToOD,
-    RGBToOD
-)
 from ._preprocessor import Preprocessor
 from ._process import Process
 
 __all__ = [
     "Preprocessor",
-    "Process",
-    "RGBToGrey",
-    "RGBToGray",
-    "GreyToOD",
-    "GrayToOD",
-    "RGBToOD"
+    "Process"
 ]
