f30c1759fcfa094e21cea2d69ed9f354538c10b9
Hugh Warden <hugh.warden@outlook.com>
2024-03-16 13:02:11 +0000

    add label dilation

--- src/scematk/process/morphology/__init__.py
+++ src/scematk/process/morphology/__init__.py
@@ -1,8 +1,10 @@
 from ._binary_morphology import BinaryClosing, BinaryDilation, BinaryErosion, BinaryOpening
+from ._label_morphology import LabelDilation
 
 __all__ = [
     "BinaryClosing",
     "BinaryDilation",
     "BinaryErosion",
     "BinaryOpening",
+    "LabelDilation"
 ]
