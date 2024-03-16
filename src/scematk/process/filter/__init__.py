b8be2bf3527af9a798434cc5eb1c6737588a6de5
Hugh Warden <hugh.warden@outlook.com>
2024-03-16 13:09:55 +0000

    add gaussian filter process

---
+++ src/scematk/process/filter/__init__.py
@@ -0,0 +1,5 @@
+from ._gaussian import GaussianFilter
+
+__all__ = [
+    "GaussianFilter"
+]
---
+++ src/scematk/process/filter/_gaussian.py
@@ -0,0 +1,20 @@
+from .._process import Process
+from dask.array import Array
+from dask_image.ndfilters import gaussian_filter
+
+class GaussianFilter(Process):
+    def __init__(self, sigma, order=0, mode='reflect', cval=0.0, truncate=4.0):
+        self.sigma = sigma
+        self.order = order
+        self.mode = mode
+        self.cval = cval
+        self.truncate = truncate
+
+    def process(self, image: Array) -> Array:
+        assert isinstance(image, Array), f"Expected image to be of type Array, got {type(image)}"
+        sigma = self.sigma
+        order = self.order
+        mode = self.mode
+        cval = self.cval
+        truncate = self.truncate
+        return gaussian_filter(image, sigma=sigma, order=order, mode=mode, cval=cval, truncate=truncate)
