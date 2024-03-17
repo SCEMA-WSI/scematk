f34c025e69ff5cc12a844e6810a6befb17b3630d
Hugh Warden <hugh.warden@outlook.com>
2024-03-17 11:41:56 +0000

    add abc segmenter default processors

--- src/scematk/segment/_segmenters.py
+++ src/scematk/segment/_segmenters.py
@@ -5,15 +5,27 @@ from dask.array import Array
 class Segmenter(ABC):
     def __init__self(self, name: str, preprocessor: Processor, postprocessor: Processor) -> None:
         assert isinstance(name, str), "Name must be a string"
-        assert isinstance(preprocessor, Processor), "Preprocessor must be an instance of Processor"
-        assert isinstance(postprocessor, Processor), "Postprocessor must be an instance of Processor"
+        assert isinstance(preprocessor, [Processor, None]), "Preprocessor must be a Processor or None"
+        assert isinstance(postprocessor, [Processor, None]), "Postprocessor must be a Processor or None"
         self.name = name
         self.fitted = False
         self.preprocessor = preprocessor
         self.postprocessor = postprocessor
+        if self.preprocessor is None:
+            self.preprocessor = self._default_preprocessor()
+        if self.postprocessor is None:
+            self.postprocessor = self._default_postprocessor()
 
     @abstractmethod
     def fit(self, image: Array) -> None:
+        pass
+
+    @abstractmethod
+    def _default_preprocessor(self) -> Processor:
+        pass
+
+    @abstractmethod
+    def _default_postprocessor(self) -> Processor:
         pass
 
 class PrimarySegmenter(Segmenter):
