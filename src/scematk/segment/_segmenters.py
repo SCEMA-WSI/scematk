6d9c8eb733efa9b57595c00d1bbc2b9d3a7257c6
Hugh Warden <hugh.warden@outlook.com>
2024-03-17 11:38:15 +0000

    add processors to segemnters

--- src/scematk/segment/_segmenters.py
+++ src/scematk/segment/_segmenters.py
@@ -1,19 +1,24 @@
+from ..process._processor import Processor
 from abc import ABC, abstractmethod
 from dask.array import Array
 
 class Segmenter(ABC):
-    def __init__self(self, name: str) -> None:
-        assert isinstance(name, str)
+    def __init__self(self, name: str, preprocessor: Processor, postprocessor: Processor) -> None:
+        assert isinstance(name, str), "Name must be a string"
+        assert isinstance(preprocessor, Processor), "Preprocessor must be an instance of Processor"
+        assert isinstance(postprocessor, Processor), "Postprocessor must be an instance of Processor"
         self.name = name
         self.fitted = False
+        self.preprocessor = preprocessor
+        self.postprocessor = postprocessor
 
     @abstractmethod
     def fit(self, image: Array) -> None:
         pass
 
 class PrimarySegmenter(Segmenter):
-    def __init__(self, name: str) ->  None:
-        super().__init__(name)
+    def __init__(self, name: str, preprocessor: Processor, postprocessor: Processor) -> None:
+        super().__init__(name, preprocessor, postprocessor)
 
     @abstractmethod
     def segment(self, image: Array) -> Array:
@@ -24,8 +29,8 @@ class PrimarySegmenter(Segmenter):
         pass
 
 class SecondarySegmenter(Segmenter):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
+    def __init__(self, name: str, preprocessor: Processor, postprocessor: Processor) -> None:
+        super().__init__(name, preprocessor, postprocessor)
 
     @abstractmethod
     def segment(self, image: Array, mask: Array) -> Array:
@@ -36,8 +41,8 @@ class SecondarySegmenter(Segmenter):
         pass
 
 class TertiarySegmenter(Segmenter):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
+    def __init__(self, name: str, preprocessor: Processor, postprocessor: Processor) -> None:
+        super().__init__(name, preprocessor, postprocessor)
 
     @abstractmethod
     def segment(self, image: Array, mask_primary: Array, mask_secondary: Array) -> Array:
