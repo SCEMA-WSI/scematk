2006a76eea724abd88820fa018afdc38440c5f5e
Hugh Warden <hugh.warden@outlook.com>
2024-03-18 09:50:09 +0000

    update rules based classifirr

--- src/scematk/segment/tissue/_rules_based.py
+++ src/scematk/segment/tissue/_rules_based.py
@@ -1,8 +1,8 @@
 from .._segmenters import PrimarySegmenter
+from ...process._processor import Processor
+from ...process.colour._hsv_transform import RGBToHSV
 import dask.array as da
 from dask.array import Array
-from scematk.process._processor import Processor
-from skimage.color import rgb2hsv
 
 class RulesBasedTissueSegmenter(PrimarySegmenter):
     def __init__(self, preprocessor: Processor = None, postprocessor: Processor = None) -> None:
@@ -14,12 +14,24 @@ class RulesBasedTissueSegmenter(PrimarySegmenter):
 
     def segment(self, image: Array) -> Array:
         image = self.preprocessor.process(image)
-        image = da.map_blocks(rgb2hsv, image, dtype=float)
+        image = RGBToHSV().process(image)
         saturation = image[:, :, 1]
         saturation = da.less_equal(saturation, 0.1)
         value = image[:, :, 2]
         value = da.greater_equal(value, 0.1)
-        mask = da.logical_and(saturation, value)
+        blank_mask = da.logical_and(saturation, value)
+        hue = image[:, :, 0]
+        hue1 = da.less(hue, 0.7)
+        hue2 = da.greater(hue, 0.4)
+        hue = da.logical_and(hue1, hue2)
+        saturation = image[:, :, 1]
+        saturation = da.greater(saturation, 0.1)
+        hue_saturation = da.logical_and(hue, saturation)
+        value = image[:, :, 2]
+        value = da.less(value, 0.1)
+        blue_mask = da.logical_or(hue_saturation, value)
+        mask = da.logical_and(blank_mask, blue_mask)
+        mask = da.logical_not(mask)
         mask = self.postprocessor.process(mask)
         return mask
 
