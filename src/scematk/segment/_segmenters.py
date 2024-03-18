2be52b244653eb3c377c85d24702985c159e22b2
Hugh Warden <hugh.warden@outlook.com>
2024-03-18 22:38:49 +0000

    reorder constructor

--- src/scematk/segment/_segmenters.py
+++ src/scematk/segment/_segmenters.py
@@ -5,16 +5,16 @@ from dask.array import Array
 class Segmenter(ABC):
     def __init__(self, name: str, preprocessor: Processor, postprocessor: Processor) -> None:
         assert isinstance(name, str), "Name must be a string"
-        assert any(isinstance(preprocessor, type) for type in (Processor, None)), "Preprocessor must be a Processor or None"
-        assert any(isinstance(postprocessor, type) for type in (Processor, None)), "Postprocessor must be a Processor or None"
+        if preprocessor is None:
+            preprocessor = self._default_preprocessor()
+        if postprocessor is None:
+            postprocessor = self._default_postprocessor()
+        assert isinstance(preprocessor, Processor), "Preprocessor must be a Processor"
+        assert isinstance(postprocessor, Processor), "Postprocessor must be a Processor"
         self.name = name
         self.fitted = False
         self.preprocessor = preprocessor
         self.postprocessor = postprocessor
-        if self.preprocessor is None:
-            self.preprocessor = self._default_preprocessor()
-        if self.postprocessor is None:
-            self.postprocessor = self._default_postprocessor()
 
     @abstractmethod
     def fit(self, image: Array) -> None:
