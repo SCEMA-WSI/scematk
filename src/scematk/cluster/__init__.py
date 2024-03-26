b4a80517b3c0762d497e1daf9d0e9a4faf76a7a1
Hugh Warden <hugh.warden@outlook.com>
2024-03-26 18:01:06 +0000

    add init

---
+++ src/scematk/cluster/__init__.py
@@ -0,0 +1,5 @@
+from ._quick_connect import quick_connect
+
+__all__ [
+    "quick_connect"
+]
--- src/scematk/cluster/_quick_connect.py
+++ src/scematk/cluster/_quick_connect.py
@@ -1,12 +1,12 @@
-from dask.distributed import Client
 from dask_jobqueue import SGECluster
 from .profiles import eddie_profile
 
-def quick_connect(profile: str) -> Client:
+def quick_connect(profile: str, min: int = 14, max: int = 16):
     assert isinstance(profile, str), "profile must be a string"
     if profile == "eddie":
         cluster = SGECluster(**eddie_profile)
-        client = Client(cluster)
-        return client
     else:
         raise ValueError(f"Profile {profile} not recognised")
+    cluster.adapt(minimum=min, maximum=max)
+    cluster.wait_for_workers(min)
+    return cluster
