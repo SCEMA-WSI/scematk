371d662d81b7847b53d397bc746386cc9518582b
Hugh Warden <hugh.warden@outlook.com>
2024-05-29 10:22:53 +0100

    Add masks to __init__.py

--- scematk/image/__init__.py
+++ scematk/image/__init__.py
@@ -1,5 +1,9 @@
+from ._binary_mask import BinaryMask
+from ._label_mask import LabelMask
 from ._ubyte_image import UByteImage
 
 __all__ = [
+    'BinaryMask',
+    'LabelMask',
     'UByteImage'
 ]
